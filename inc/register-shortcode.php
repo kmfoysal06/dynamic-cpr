ab576781bce9b202984d86697e918f366cb69ac5
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-12-20 18:21:27 +0600

    updated

--- inc/register-shortcode.php
+++ inc/register-shortcode.php
@@ -20,19 +20,60 @@
 function kmf_cpr_save_or_update_post($post_id, $post, $update) {
     // Check if the post type is 'custom_post_type'
     if ($post->post_type === 'cpr') {
-		add_action('init','kmf_cpr_custom_post');
-		function kmf_custom_post(){
-			register_post_type("great",[
-		'labels' => [
+		// function kmf_custom_post(){
+	// 		register_post_type("great",[
+	// 	'labels' => [
+	// 		'name' => __('Great'),
+	// 	],
+	// 	'public' => true,
+	// 	'supports' => ['title'],
+	// 		]);
+	// 	}
+	 // add_action('init','kmf_custom_post');
+	// $posts = get_post_meta($post_id,'post_types');
+	// if(is_array($posts) && !empty($posts)){
+    // foreach ($posts as $post) { 
+    // 	$postdt = print_r($posts);
+    // 	 wp_die('Custom post type actions executed successfully!'.$postdt);
+    // }
+	//  }else{
+	//  	wp_die();
+	//  }
+
+
+
+function cp(){
+	register_post_type('great',[
+		'labels'=>[
 			'name' => __('Great'),
 		],
 		'public' => true,
 		'supports' => ['title'],
-			]);
-		}
-	 add_action('init','kmf_custom_post');
-	 }
+	]);
+}
+if(!add_action('init','cp')){
+	wp_die('some fuking error');
+}
+}
 
 }
 
 add_action('save_post', 'kmf_cpr_save_or_update_post', 10, 3);
+
+
+
+// [ 
+// 	[0] => [ 
+// 		[0] => [
+// 		 [type] => post 
+// 		 [post_info] => 
+// 		 	[ 
+// 		 		[0] => 
+// 		 		[ 
+// 		 			[post_type_id] => great 
+// 		 			[ispublic] => 1 
+// 		 			[show_ui] => ] 
+// 		 	] 
+// 		]
+// 		  ]
+// ]
