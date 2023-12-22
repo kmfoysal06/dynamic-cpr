b238dc1ce613573203470df99d16ce483adff5a3
kazi mohammed foysal <foysalkazimd01@gmail.com>
2023-12-22 14:22:15 +0600

    updated

--- inc/great.php
+++ inc/great.php
@@ -1,5 +1,5 @@
 <?php
-function cp(){register_post_type('kmf_store',['labels'=>['name' => __('KMF STORE'),],'public' => true,'show_ui' => true,'supports' => ['title','editor',]	
+function cp(){register_post_type('kmf_store',['labels'=>['name' => __('KMF STORE'),],'public' => false,'show_ui' => false,'supports' => ['title','editor',]	
 	]);
 }
 add_action('init','cp');
