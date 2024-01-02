f5351f5df070ebb8046b32e06e96d2f5b34cc715
kazi mohammed foysal <foysalkazimd01@gmail.com>
2024-01-02 17:14:53 +0600

    successfully added custom metabox

--- cpr.php
+++ cpr.php
@@ -7,9 +7,8 @@ Author: Kazi Mohammad Foysal
 Author URI: https://www.github.com/kmfoysal06
 Text Domain: kmf-custom-post
 */
-require_once plugin_dir_path(__FILE__)."libs/fields/codestar-framework.php";
+
 require_once plugin_dir_path(__FILE__)."inc/custom-post.inc.php";
-require_once plugin_dir_path(__FILE__)."inc/metaboxes.php";
 require_once plugin_dir_path(__FILE__)."inc/custom-metaboxes.inc.php";
 require_once plugin_dir_path(__FILE__)."inc/register-post-types-custom-metas.inc.php";
 require_once plugin_dir_path(__FILE__)."inc/create_post.inc.php";
