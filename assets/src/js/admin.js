e03ab73cc52af564cbe2b4fc444fd679ece039c7
kmfoysal06 <foysalkazimd01@gmail.com>
2025-05-07 07:55:58 +0600

    added dynamic option values retrieve and add row functionalty

--- assets/src/js/admin.js
+++ assets/src/js/admin.js
@@ -1,14 +1,55 @@
 jQuery(document).ready(function($) {
     // show hide single post type
     $('.single-post-type').each(function(){
-        let title = $(this).find('h2');
-
         let fields = $(this).find('.fields-container');
         fields.hide();
-
-        title.on('click', function(){
-            fields.slideToggle();
-            $(this).toggleClass('active');
-        });
     })
+
+    $(document).on("click", ".single-post-type h2", function() {
+        const title = $(this).closest("h2");
+        const fields = title.closest(".single-post-type").find(".fields-container");
+        fields.slideToggle();
+        $(this).toggleClass("active")
+    })
+
+    $('.kmfdcpr-field .add-row').on("click", function() {
+        // add new row
+        let placeholderRow = $(".kmfdcpr-admin-container .single-post-type.placeholder");
+        const postTypeContainer = $(".kmfdcpr-admin-container .post-types-container")
+
+        let newPostType = placeholderRow.clone(true).removeClass("placeholder").css("display", "");
+        postTypeContainer.append(newPostType)
+
+    })
+
+    $('.kmfdcpr-field .save').on("click", function() {
+        // process a ajax request to save data
+        const postTypeContainer = $(".kmfdcpr-admin-container .post-types-container")
+        let postTypesData = [];
+        $('.single-post-type').each(function(){
+            const postTypeId = $(this).find(".pt input").val();
+            const postTypeTitle = $(this).find(".name input").val();
+            const postTypePublic = $(this).find(".ip input").val();
+            const postTypeAdmin = $(this).find(".su input").val();
+            const postTypeSupports = $(this).find(".sup input").val();
+            if(!postTypeTitle.length) {
+                kmfdcpr_alert("post type name required!")
+                return;
+            }
+            const singlePostType = {
+                'id' : postTypeId,
+                'title' : postTypeTitle,
+                'is_public' : postTypePublic,
+                'is_for_admin' : postTypeAdmin,
+                'supports' : postTypeSupports
+            }
+            postTypesData.push(singlePostType)
+
+        })
+        console.log(postTypesData)
+    })
+
+    function kmfdcpr_alert(text) {
+        alert(text)
+    }
 });
--- templates/settings.php
+++ templates/settings.php
@@ -11,75 +11,144 @@ exit; // Exit if accessed directly.
 }
 $post_type_id = 'a';
 $post_type_title = 'Books';
-
+$post_types = [
+    ['id' => 'a', 'title' => 'Books'], 
+    ['id' => 'b', 'title' => 'Articles'], 
+]
 ?>
 
 <div class="kmfdcpr-admin-container kmfdcpr-settings">
-<h1>Dynamic CPR Settings Page</h1>
+    <h1>Dynamic CPR Settings Page</h1>
 
     <div class="post-types-container">
-        <div class="single-post-type">
-            <h2><?php echo esc_html($post_type_title); ?></h2>
+        <!-- placeholder post type for using on add-row -->
+        <div class="single-post-type placeholder" style="display: none">
+            <h2>Book</h2>
             <div class="fields-container">
 
-               <?php wp_nonce_field(basename(__FILE__), 'kmfdcpr_meta_nonce'); ?>
+                <?php wp_nonce_field(basename(__FILE__), 'kmfdcpr_meta_nonce'); ?>
 
-            <p><b>post type id and name must be unique and only contain alphanumeric characters and underscores and length should be less than 20</b></p>
-            <div class="kmfdcpr-field pt">
-              <label class="col-25" for="pt">Post Type ID</label>
-              <input type="text" id="pt" name="kmfcpr_metadata[cpr_id]" value=""  required>
-            </div>
+                <p><b>post type id and name must be unique and only contain alphanumeric characters and underscores and length should be less than 20</b></p>
+                <div class="kmfdcpr-field pt">
+                    <label class="col-25" for="pt">Post Type ID</label>
+                    <input type="text" id="pt" name="kmfcpr_metadata[cpr_id]" value=""  required>
+                </div>
 
-            <div class="kmfdcpr-field name">
-              <label class="col-25" for="name">Post Type Name</label>
-              <input type="text" id="name" name="kmfcpr_metadata[cpr_name]" value="" required >
-            </div>
+                <div class="kmfdcpr-field name">
+                    <label class="col-25" for="name">Post Type Name</label>
+                    <input type="text" id="name" name="kmfcpr_metadata[cpr_name]" value="" required >
+                </div>
 
 
-            <div class="kmfdcpr-field ip">
-                <input type="checkbox" id="ip" name="kmfcpr_metadata[ip]" >
-                <label for="ip">is Public</label>
-            </div>
+                <div class="kmfdcpr-field ip">
+                    <input type="checkbox" id="ip" name="kmfcpr_metadata[ip]" >
+                    <label for="ip">is Public</label>
+                </div>
+        
+                <div class="kmfdcpr-field su">
+                    <input type="checkbox" id="su" name="kmfcpr_metadata[su]" >
+                    <label for="su">Show UI</label>
+                </div>
     
-            <div class="kmfdcpr-field su">
-                <input type="checkbox" id="su" name="kmfcpr_metadata[su]" >
-                <label for="su">Show UI</label>
-            </div>
+                <div class="kmfdcpr-field sup">
+                    <p><span title="If you don't select anything, the default supports (title and editor) will be added.">Supports</span></p>
+                    <div class="inputs">
+                        <input type="checkbox" id="meta-title" name="kmfcpr_metadata[supports][]" value="title" >
+                        <label for="meta-title">Title</label>
     
-            <div class="kmfdcpr-field sup">
-                <p><span title="If you don't select anything, the default supports (title and editor) will be added.">Supports</span></p>
-                <div class="inputs">
-                <input type="checkbox" id="meta-title" name="kmfcpr_metadata[supports][]" value="title" >
-                <label for="meta-title">Title</label>
+                        <input type="checkbox" id="thumbnail" name="kmfcpr_metadata[supports][]'" value="thumbnail" >
+                        <label for="thumbnail">Thumbnail</label>
 
-               <input type="checkbox" id="thumbnail" name="kmfcpr_metadata[supports][]'" value="thumbnail" >
-                <label for="thumbnail">Thumbnail</label>
+                        <input type="checkbox" id="editor" name="kmfcpr_metadata[supports][]'" value="editor" >
+                        <label for="editor">Editor</label>
 
-                <input type="checkbox" id="editor" name="kmfcpr_metadata[supports][]'" value="editor" >
-                <label for="editor">Editor</label>
+                        <input type="checkbox" id="comments" name="kmfcpr_metadata[supports][]" value="comments" >
+                        <label for="comments">Comments</label>
 
-                <input type="checkbox" id="comments" name="kmfcpr_metadata[supports][]" value="comments" >
-                <label for="comments">Comments</label>
-
-                <input type="checkbox" id="custom-fields" name="kmfcpr_metadata[supports][]" value="custom-fields" >
-                <label for="custom-fields">Custom Fields</label>
+                        <input type="checkbox" id="custom-fields" name="kmfcpr_metadata[supports][]" value="custom-fields" >
+                        <label for="custom-fields">Custom Fields</label>
 
 
-                <input type="checkbox" id="author" name="kmfcpr_metadata[supports][]'" value="author" >
-                <label for="author">Author</label>
+                        <input type="checkbox" id="author" name="kmfcpr_metadata[supports][]'" value="author" >
+                        <label for="author">Author</label>
 
-                <input type="checkbox" id="post-formats" name="kmfcpr_metadata[supports][]" value="post-formats" >
-                <label for="post-formats">Post Formats</label>
+                        <input type="checkbox" id="post-formats" name="kmfcpr_metadata[supports][]" value="post-formats" >
+                        <label for="post-formats">Post Formats</label>
 
 
-                <input type="checkbox" id="page-attributes" name="kmfcpr_metadata[supports][]'" value="page-attributes" >
-                <label for="page-attributes">Page Attributes</label>
+                        <input type="checkbox" id="page-attributes" name="kmfcpr_metadata[supports][]'" value="page-attributes" >
+                        <label for="page-attributes">Page Attributes</label>
+                    </div>
                 </div>
-                </div>
-                </div>
+            </div>
         </div>
+
+        <?php foreach($post_types as $post_type): ?>
+            <div class="single-post-type">
+                <h2><?php echo esc_html($post_type["title"]); ?></h2>
+                <div class="fields-container">
+
+                    <?php wp_nonce_field(basename(__FILE__), 'kmfdcpr_meta_nonce'); ?>
+
+                    <p><b>post type id and name must be unique and only contain alphanumeric characters and underscores and length should be less than 20</b></p>
+                    <div class="kmfdcpr-field pt">
+                      <label class="col-25" for="pt">Post Type ID</label>
+                      <input type="text" id="pt" name="kmfcpr_metadata[cpr_id]" value=""  required>
+                    </div>
+
+                    <div class="kmfdcpr-field name">
+                      <label class="col-25" for="name">Post Type Name</label>
+                      <input type="text" id="name" name="kmfcpr_metadata[cpr_name]" value="" required >
+                    </div>
+
+
+                    <div class="kmfdcpr-field ip">
+                        <input type="checkbox" id="ip" name="kmfcpr_metadata[ip]" >
+                        <label for="ip">is Public</label>
+                    </div>
+        
+                    <div class="kmfdcpr-field su">
+                        <input type="checkbox" id="su" name="kmfcpr_metadata[su]" >
+                        <label for="su">Show UI</label>
+                    </div>
+        
+                    <div class="kmfdcpr-field sup">
+                        <p><span title="If you don't select anything, the default supports (title and editor) will be added.">Supports</span></p>
+                        <div class="inputs">
+                          <input type="checkbox" id="meta-title" name="kmfcpr_metadata[supports][]" value="title" >
+                          <label for="meta-title">Title</label>
+
+                          <input type="checkbox" id="thumbnail" name="kmfcpr_metadata[supports][]'" value="thumbnail" >
+                          <label for="thumbnail">Thumbnail</label>
+
+                          <input type="checkbox" id="editor" name="kmfcpr_metadata[supports][]'" value="editor" >
+                          <label for="editor">Editor</label>
+
+                          <input type="checkbox" id="comments" name="kmfcpr_metadata[supports][]" value="comments" >
+                          <label for="comments">Comments</label>
+
+                          <input type="checkbox" id="custom-fields" name="kmfcpr_metadata[supports][]" value="custom-fields" >
+                          <label for="custom-fields">Custom Fields</label>
+
+
+                          <input type="checkbox" id="author" name="kmfcpr_metadata[supports][]'" value="author" >
+                          <label for="author">Author</label>
+
+                          <input type="checkbox" id="post-formats" name="kmfcpr_metadata[supports][]" value="post-formats" >
+                          <label for="post-formats">Post Formats</label>
+
+
+                          <input type="checkbox" id="page-attributes" name="kmfcpr_metadata[supports][]'" value="page-attributes" >
+                          <label for="page-attributes">Page Attributes</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?> 
     </div>
-    <div class="kmfdcpr-field">
-        <button type="submit" class="button button-primary">Add New Post Type</button>
+    <div class="kmfdcpr-field action-field">
+        <button type="button" class="button button-primary add-row">Add New Post Type</button>
+        <button type="submit" class="button button-primary save">Save</button>
     </div>
+
 </div>
